11dc5c7ebe7fae6ba908f0c603a90eb7e2c54a57
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-12-25 00:25:17 +0100

    updated readme

--- src/Facades/Buckaroo.php
+++ src/Facades/Buckaroo.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Facade;
 
 class Buckaroo extends Facade
 {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor(): string
     {
         return 'Buckaroo';
